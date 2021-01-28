4a78b4605e277cfef1486e6df558bf1cf8a4e636
124um <a.d.katar@gmail.com>
2021-01-28 21:45:26 +0300

    include mogoose

--- server_shelf/models/book.js
+++ server_shelf/models/book.js
@@ -4,7 +4,7 @@ const Schema = mongoose.Schema;
 const BookSchema = new Schema({
   title: { type: String, default: '' },
   description: { type: String, default: '' },
-  coverId: { type: Schema.Types.ObjectId, ref:'Cover'}
+  coverId: { type: Number}
 });
 
 module.exports = mongoose.model('Book', BookSchema);
--- server_shelf/utils/BooksUtils.js
+++ server_shelf/utils/BooksUtils.js
@@ -8,18 +8,14 @@ const coversTestData = require( '../data/covers.json')
 const Book = require("../models/book")
 const Cover = require("../models/cover")
 
-
-mongoose.connect("mongodb://librarian:libpassword@localhost:27017/Books?authSource=admin", { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false }, function(err){
+mongoose.connect("mongodb://librarian:libpassword@localhost:27017/LIBRARY?authSource=admin&readPreference=primary", { useUnifiedTopology: true, useNewUrlParser: true, useFindAndModify: false }, function(err){
         if(err) return console.log(err);
 });
 
 
-
 exports.getBooksData = async () => {
-    const books = Book.find({id:1})
-    // return books
-    console.log("exports.getBooksData= ~ books---------------------------->", books)
-    return books;
+    const books = Book.find({})
+    return books
 };
 
 exports.getBookData = async ( bookId ) => {
@@ -29,14 +25,12 @@ exports.getBookData = async ( bookId ) => {
 
 exports.postBookData = async (newData) => {
     const book = Book.create({ title : newData.title , description : newData. description, coverId: newData.coverId });
-    await book.save();
-    return book;
+    return book.save();
 };
 
 exports.patchBookData = async (newData) => {
     const book = Book.findByIdAndUpdate( newData.id, { title : newData.title , description : newData. description, coverId: newData.coverId });
-    await book.save();
-    return book;
+    return book.save();
 };
 
 exports.deleteBookData = async (bookId) => {
@@ -54,14 +48,12 @@ exports.getCoverData = async (coverId) => {
 
 exports.postCoverData = async (newData) => {
     const cover = Cover.create({ name: newData.name });
-    await cover.save();
-    return cover;    
+    return cover.save();    
 };
 
 exports.patchCoverData = async (newData) => {
     const cover = Cover.findByIdAndUpdate(newData.id, { name: newData.name });
-    await cover.save();
-    return cover;
+    return cover.save();
 }; 
 
 exports.deleteCoverData = async (coverId) => {
